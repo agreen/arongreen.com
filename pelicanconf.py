6a4d7e1fc135738bc48cfafc130c2a97980c22c4
Aron Green <aron@arongreen.com>
2025-02-01 23:58:11 -0500

    Working m.css theme with submodule properly configured

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,21 +1,68 @@
+# ===========================
+# Basic Site Information
+# ===========================
 AUTHOR = 'Aron Green'
 SITENAME = 'Aron Green (dot) com'
-SITEURL = ""
 
-PATH = "content"
-
+# ===========================
+# Localization and Timezone
+# ===========================
 TIMEZONE = 'America/New_York'
-
 DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
+# ===========================
+# Content and Output Settings
+# ===========================
+PATH = "content"
+DEFAULT_PAGINATION = 10
+DIRECT_TEMPLATES = ['index']
+
+# ===========================
+# URL and Site Settings
+# ===========================
+SITEURL = ""
+RELATIVE_URLS = True  # Ensure relative URLs for development
+
+# ===========================
+# Theme and Appearance
+# ===========================
+THEME = 'theme/m.css/pelican-theme'
+THEME_STATIC_DIR = 'static'
+
+# ===========================
+# Static Files and CSS
+# ===========================
+STATIC_PATHS = [
+    'theme/m.css/pelican-theme/static',
+    'static'
+]
+
+M_CSS_FILES = [
+    'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i|Source+Code+Pro:400,400i,600',
+    '/static/m-light.css',  # Use '/static/m-dark.css' for dark mode if preferred
+    '/static/m-theme-arongreen.css'  # Custom overrides
+]
+
+M_THEME_COLOR = '#006400'  # Dark green for your color palette
+
+# ===========================
+# Plugins
+# ===========================
+PLUGIN_PATHS = ['theme/m.css/plugins']
+PLUGINS = ['m.htmlsanity', 'm.components', 'm.images', 'm.code', 'm.math']
+
+# ===========================
+# Feed Settings (disabled for development)
+# ===========================
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
+# ===========================
+# Blogroll (external links)
+# ===========================
 LINKS = (
     ("Pelican", "https://getpelican.com/"),
     ("Python.org", "https://www.python.org/"),
@@ -23,13 +70,13 @@ LINKS = (
     ("You can modify those links in your config file", "#"),
 )
 
-# Social widget
+# ===========================
+# Social Widget
+# ===========================
 SOCIAL = (
     ("You can add links in your config file", "#"),
     ("Another social link", "#"),
 )
-
-DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
